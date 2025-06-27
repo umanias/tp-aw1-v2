23e8227d13d02e035cb1c1e233d37c406571682d
umanias <ulises.mmanias04@gmail.com>
2025-06-27 01:49:22 -0300

    nashe

--- server/app.mjs
+++ server/app.mjs
@@ -2,10 +2,11 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+import apiRouter from './routes/api.mjs';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PUERTO = process.env.PUERTO || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -15,6 +16,8 @@ app.use(express.static(path.join(__dirname, '../html')));
 
 app.use('/resources', express.static(path.join(__dirname, '../resources')));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PUERTO, () => {
+    console.log(`Server is running on http://localhost:${PUERTO}`);
 });
+
+
---
+++ server/routes/api.mjs
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+import fs from 'fs/promises';
+
+const router = express.Router();
+
+let productos = [];
+
+const filePath = path.resolve('./resources/datos/tienda.json');
+
+async function cargarProductos() {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    productos = JSON.parse(data);
+  } catch (error) {
+    console.error('Error cargando productos:', error);
+  }
+}
+
+await cargarProductos();
+
+router.get('/', (req, res) => {
+  res.json(productos);
+});
+
+router.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const producto = productos.find(p => p.id === id);
+
+  if (producto) res.json(producto);
+  else res.status(404).json({ error: "Producto no encontrado" });
+});
+
+export default router;
